dc55da3b86045c673ba22dd4f7cf53966fece24d
SeaTimeMoon <yang.hu@leica-geosystems.com.cn>
2018-04-23 16:49:27 +0800

    chapter 12 completed

--- chapter12/scripts/global.js
+++ chapter12/scripts/global.js
@@ -293,6 +293,83 @@ function displayAbbreviations(){
     container.appendChild(dlist);
 }
 
+function focusLabels() {
+    var labels = document.getElementsByTagName("label");
+    for(var i=0;i<labels.length;i++){
+        if(!lables[i].getAttribute("for")) continue;
+        labels[i].onclick = function() {
+            var id = this.getAttribute("for");
+            if(!document.getElementById(id)) return false;
+            var element = document.getElementById(id);
+            element.focus();
+        }
+    }
+}
+
+function resetFields(whichform) {
+    for(var i=0; i<whichform.elements.length;i++){
+        var element = whichform.elements[i];
+        if(element.type == "submit") continue;
+        var check = element.placeholder || element.getAttribute("placeholder");
+        if(!check) continue;
+        element.onfocus = function() {
+            var text = this.placeholder || this.getAttribute("placeholder");
+            if(this.value == text){
+                this.className = "";
+                this.value = "";
+            }
+        }
+        element.onblur = function() {
+            if(this.value==""){
+                this.className = "placeholder";
+                this.value = this.placeholder || this.getAttribute("placeholder");
+            }
+        }
+        element.onblur();
+    }
+}
+
+
+
+function isFilled(field) {
+    if(field.value.replace(" ","").length == 0)return false;
+    var placeholder = field.placeholder || field.getAttribute("placeholder");
+    return (field.value != placeholder);
+}
+
+function isEmail(field) {
+    return (field.value.indexOf("@")!=-1 && 
+    field.value.indexOf(".")!=-1);
+}
+
+function validateForm(whichform) {
+    for(var i=0;i<whichform.elements.length;i++){
+        var element = whichform.elements[i];
+        if(element.required == "required") {
+            if(!isFilled(element)){
+                alert("Please fill in the "+element.name+" field.");
+                return false;
+            }
+        }
+        if(element.type=="email"){
+            if(!isEmail(element)){
+                alert("The "+element.name+" field must be a valid  email address.")
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function prepareForms() {
+    for(var i=0; i<document.forms.length;i++){
+        var thisform = document.forms[i];
+        resetFields(thisform);
+        thisform.onsubmit = function() {
+            return validateForm(this);
+        }
+    }
+}
 
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
@@ -302,3 +379,5 @@ addLoadEvent(prepareGallery);
 addLoadEvent(stripeTables);
 addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
+addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
